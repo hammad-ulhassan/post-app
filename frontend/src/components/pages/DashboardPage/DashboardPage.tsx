9a208be0e44dd97119bfe4fd9571a363775c7954
Hammad Hassan <hammad.hassan.murtaza@gmail.com>
2025-06-03 20:47:14 +0500

    main layout

--- frontend/src/components/pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/components/pages/DashboardPage/DashboardPage.tsx
@@ -1,31 +1,105 @@
-import { Card, Menu, type MenuProps } from 'antd';
-import { useState } from 'react';
+import {
+  Button,
+  Card,
+  Dropdown,
+  Flex,
+  List,
+  Menu,
+  Space,
+  type MenuProps,
+} from "antd";
+import { Content } from "antd/es/layout/layout";
+import { useState } from "react";
+import ListItem from "./ListItem";
+import { useGetUserPostsQuery } from "../../../services/user";
+import { SortAscendingOutlined } from "@ant-design/icons";
 
 const items = [
   {
-    label: 'All Posts',
-    key: 'all',
+    label: "All Posts",
+    key: "all",
   },
   {
-    label: 'Latest Posts',
-    key: 'latest',
+    label: "Latest Posts",
+    key: "latest",
   },
   {
-    label: 'Archived',
-    key: 'archived',
+    label: "Archived",
+    key: "archived",
   },
 ];
-const DashboardPage = () => {
-  const [current, setCurrent] = useState('all');
 
-  const onClick: MenuProps['onClick'] = (e) => {
-    console.log('click ', e);
+const handleMenuClick: MenuProps["onClick"] = (e) => {
+  console.log("click", e);
+};
+
+const dropDownItems: MenuProps["items"] = [
+  {
+    label: "Title",
+    key: "1",
+  },
+  {
+    label: "User",
+    key: "2",
+  },
+];
+
+const menuProps = {
+  items: dropDownItems,
+  onClick: handleMenuClick,
+};
+
+const DashboardPage = () => {
+  const [current, setCurrent] = useState("all");
+  const {
+    data: userPosts,
+    isLoading: userPostsLoading,
+    isFetching: userPostsFetching,
+  } = useGetUserPostsQuery(null);
+
+  const onClick: MenuProps["onClick"] = (e) => {
     setCurrent(e.key);
   };
   return (
-    <Card>
-      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
-    </Card>
+    <>
+      <Flex
+        justify="space-between"
+        align="flex-start"
+        style={{ marginBottom: "1em" }}
+      >
+        <div>sdfsdfs</div>
+        <Dropdown menu={menuProps}>
+          <Button>
+            <Space>
+              Filter/Sort by
+              <SortAscendingOutlined />
+            </Space>
+          </Button>
+        </Dropdown>
+      </Flex>
+      <Card>
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+        />
+        <Content style={{ padding: "1em" }}>
+          <List
+            itemLayout="horizontal"
+            dataSource={userPosts}
+            renderItem={(item) => <ListItem post={item} />}
+            loading={userPostsLoading || userPostsFetching}
+            pagination={{
+              onChange: (page) => {
+                console.log(page);
+              },
+              pageSize: 4,
+            }}
+          />
+        </Content>
+      </Card>
+    </>
   );
 };
 
---
+++ frontend/src/components/pages/DashboardPage/ListItem.tsx
@@ -0,0 +1,25 @@
+import { Image, List } from "antd"
+import type { Post } from "../../../models/post"
+
+interface ListItemProps {
+    post: Post
+}
+
+const ListItem = ({ post }: ListItemProps) => {
+    return (
+        <List.Item>
+              <List.Item.Meta
+                avatar={
+                  <Image
+                    width={120}
+                    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                  />
+                }
+                title={<a href="https://ant.design">{post.title}</a>}
+                description={ post.body }
+              />
+            </List.Item>
+    )
+}
+
+export default ListItem
